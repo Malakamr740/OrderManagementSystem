ca0651edb44b208bcc5bbd5a09bf337942d32475
pc <pc@DESKTOP-VLVULJ4>
2024-09-02 15:32:29 +0300

    Add

--- OrderBase/obj/Release/net8.0/OrderBase.AssemblyInfo.cs
+++ OrderBase/obj/Release/net8.0/OrderBase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderBase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f256e70400c33e56b726cbef837fd55e5f75d7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderBase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderBase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
